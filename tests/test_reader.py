7e500b1c1c098f357b464186b14152c3d49f2d8a
Jędrzej Boczar <jboczar@antmicro.com>
2021-01-08 11:46:37 +0100

    Add test of BIST row hammer attack pattern

--- tests/test_reader.py
+++ tests/test_reader.py
@@ -1,55 +1,76 @@
 import unittest
+import itertools
 
 from migen import *
 from litedram.common import LiteDRAMNativePort
 from rowhammer_tester.gateware.bist import Reader, PatternMemory
 
+
+class BISTReaderDUT(Module):
+    def __init__(self, address_width=32, data_width=128, pattern_mem_length=32):
+        self.dram_port = LiteDRAMNativePort(address_width=address_width, data_width=data_width, mode='both')
+        self.submodules.pattern_mem = PatternMemory(self.dram_port.data_width, pattern_mem_length)
+        self.submodules.reader = Reader(self.dram_port, self.pattern_mem)
+        self.reader.add_csrs()
+
+        self.data = self.pattern_mem.data.get_port(write_capable=True)
+        self.addr = self.pattern_mem.addr.get_port(write_capable=True)
+        self.specials += self.data, self.addr
+
+    def mem_write(self, mem, addr, data):
+        yield mem.we.eq(1)
+        yield mem.adr.eq(addr)
+        yield mem.dat_w.eq(data)
+        yield
+        yield mem.we.eq(0)
+
+    @passive
+    def read_generator(self, data_callback, read_delay=1):
+        if not callable(data_callback):  # passed a single value to always be returned
+            data = data_callback
+            data_callback = lambda addr: data
+
+        while True:
+            while not (yield self.dram_port.cmd.valid):
+                yield
+
+            assert not (yield self.dram_port.cmd.we), 'WRITE command found'
+            addr = (yield self.dram_port.cmd.addr)
+
+            yield self.dram_port.cmd.ready.eq(1)
+            yield
+            yield self.dram_port.cmd.ready.eq(0)
+
+            for _ in range(read_delay):
+                yield
+
+            data = data_callback(addr)
+            yield self.dram_port.rdata.data.eq(data)
+            yield self.dram_port.rdata.valid.eq(1)
+            yield
+            while not (yield self.dram_port.rdata.ready):
+                yield
+            yield self.dram_port.rdata.valid.eq(0)
+            yield
+
+
 class TestReader(unittest.TestCase):
-    def test_pattern_1(self):
-        class DUT(Module):
-            def __init__(self):
-                self.dram_port = LiteDRAMNativePort(address_width=32, data_width=32*4, mode='both')
-                self.submodules.pattern_mem = PatternMemory(self.dram_port.data_width, 16)
-                self.submodules.reader = Reader(self.dram_port, self.pattern_mem)
-                self.reader.add_csrs()
-
-                self.data = self.pattern_mem.data.get_port(write_capable=True)
-                self.addr = self.pattern_mem.addr.get_port(write_capable=True)
-                self.specials += self.data, self.addr
-
-
+    def test_error_detection(self):
         def generator(dut):
             yield from dut.reader._count.write(8)
-
             yield from dut.reader._mem_mask.write(0xffffffff)
             yield from dut.reader._data_mask.write(0x00000000)
             yield from dut.reader._skip_fifo.write(0)
 
-            yield dut.data.adr.eq(0x0)
-            yield dut.data.dat_w.eq(0xffffffffffffffffffffffffffffffff)
-            yield dut.data.we.eq(1)
-            yield
-            yield dut.data.we.eq(0)
-            yield
-
-            yield dut.addr.adr.eq(0x0)
-            yield dut.addr.dat_w.eq(0)
-            yield dut.addr.we.eq(1)
-            yield
-            yield dut.addr.we.eq(0)
-            yield
+            yield from dut.mem_write(dut.data, addr=0, data=0xffffffffffffffffffffffffffffffff)
+            yield from dut.mem_write(dut.addr, addr=0, data=0)
 
             # Fifo should be empty
             self.assertEqual((yield from dut.reader._error_ready.read()), 0)
 
             yield from dut.reader._start.write(1)
 
-            # Fixme sync with read_handler
-            for n in range(0, 200):
-                yield
-
-            #ptr = yield from dut.reader.pointer.read()
-            #self.assertEqual(ptr, 5)
+            for _ in range(50): yield
 
             # Check if ready
             self.assertEqual((yield from dut.reader._done.read()), 8)
@@ -61,7 +82,7 @@ class TestReader(unittest.TestCase):
             self.assertEqual((yield from dut.reader._error_expected.read()), 0xffffffffffffffffffffffffffffffff)
             yield from dut.reader._error_continue.write(1)
 
-            for n in range(0, 13): yield
+            for _ in range(0, 13): yield
 
             self.assertEqual((yield from dut.reader._error_ready.read()), 1)
             self.assertEqual((yield from dut.reader._error_offset.read()), 5)
@@ -69,51 +90,62 @@ class TestReader(unittest.TestCase):
             self.assertEqual((yield from dut.reader._error_expected.read()), 0xffffffffffffffffffffffffffffffff)
             yield from dut.reader._error_continue.write(1)
 
-            for n in range(0, 17): yield
+            for _ in range(0, 17): yield
 
             self.assertEqual((yield from dut.reader._error_ready.read()), 0)
 
-            for n in range(0, 23): yield
+        def data_callback(addr):
+            if addr in [0x5, 0x3]:
+                return 0xfffffffffffffffeffffffffffffffff
+            return 0xffffffffffffffffffffffffffffffff
 
-        @passive
-        def read_handler(dram_port):
-            address = 0
-            pending = 0
-            yield dram_port.cmd.ready.eq(0)
-            while True:
-                yield dram_port.rdata.valid.eq(0)
-                if pending:
-                    yield dram_port.rdata.valid.eq(1)
-                    #yield dram_port.rdata.data.eq(self._read(address))
-
-                    #print('RD: {:08x}'.format(address))
-
-                    # FIXME: Make this more generic and use counter_shift
-                    if address == 0x5 or address == 0x3:
-                        yield dram_port.rdata.data.eq(0xfffffffffffffffeffffffffffffffff)
-                    else:
-                        yield dram_port.rdata.data.eq(0xffffffffffffffffffffffffffffffff)
-
-                    yield
-                    yield dram_port.rdata.valid.eq(0)
-                    yield dram_port.rdata.data.eq(0)
-                    pending = 0
-                elif (yield dram_port.cmd.valid):
-                    pending = not (yield dram_port.cmd.we)
-                    address = (yield dram_port.cmd.addr)
-                    if pending:
-                        yield dram_port.cmd.ready.eq(1)
-                        yield
-                        yield dram_port.cmd.ready.eq(0)
-                yield
-
-
-        dut = DUT()
-        dut.finalize()
-
+        dut = BISTReaderDUT()
         generators = [
             generator(dut),
-            read_handler(dut.dram_port),
+            dut.read_generator(data_callback),
+        ]
+        run_simulation(dut, generators)
+
+    def test_row_hammer_attack_pattern(self):
+        count = 13
+        row_addresses = [
+            0x01, 0x03, 0x07, 0x03,
         ]
 
+        def generator(dut):
+            yield from dut.reader._skip_fifo.write(1)  # not checking errors here
+            yield from dut.reader._count.write(count)
+
+            # do not increment DRAM memory addresses
+            yield from dut.reader._mem_mask.write(0)
+
+            # mask to cycle through row_addresses
+            assert len(row_addresses) & (len(row_addresses) - 1) == 0, 'Pattern length must be power of 2'
+            yield from dut.reader._data_mask.write(len(row_addresses) - 1)
+
+            for i, addr in enumerate(row_addresses):
+                yield from dut.mem_write(dut.addr, addr=i, data=addr)
+
+            yield from dut.reader._start.write(1)
+            yield from dut.reader._start.write(0)
+
+            while not (yield from dut.reader._ready.read()):
+                yield
+
+        accesses = []
+
+        def data_callback(addr):
+            accesses.append(addr)
+            return 0  # we don't care about data here
+
+        dut = BISTReaderDUT()
+        generators = [
+            generator(dut),
+            dut.read_generator(data_callback),
+        ]
         run_simulation(dut, generators)
+
+        # BIST Reader should cycle through the list of addresses
+        expected = itertools.cycle(row_addresses)
+        expected = [next(expected) for _ in range(count)]
+        self.assertEqual(accesses, expected)
