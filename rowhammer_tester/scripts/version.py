c0f4bee43cae97a9b99be43d47a8fe917b18346f
Tomasz Jurtsch <tjurtsch@antmicro.com>
2021-02-25 14:52:12 +0100

    Store build info in LiteX SoC identification memory

--- rowhammer_tester/scripts/version.py
+++ rowhammer_tester/scripts/version.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python3
 
-from rowhammer_tester.scripts.utils import RemoteClient
+from rowhammer_tester.scripts.utils import RemoteClient, memread
 
 if __name__ == "__main__":
     wb = RemoteClient()
     wb.open()
 
-    # Read CSR register containing git revision
-    print("{:08x}".format(wb.regs.buildinfo_hash.read()))
+    # Maximal identification info size is 256
+    buildinfo = memread(wb, 256, wb.bases.identifier_mem)
+
+    # Info is stored as a \0 terminated string
+    # truncate it
+    string_term_idx = buildinfo.index(0)
+    buildinfo = buildinfo[:string_term_idx]
+
+    # Map int values to ASCII characters
+    print(''.join(map(chr, buildinfo)))
 
     wb.close()
